4aef83cf592391f44ae377ad2d910fc623cd5fc5
Lucian Poston <lucian.poston@gmail.com>
2018-01-20 10:12:03 -0800

    Remove workaroundCreepyXServer
    Fixes #5
    Fixes #8

--- xprintidle.c
+++ xprintidle.c
@@ -24,10 +24,6 @@ along with this program; if not, write to the
 Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
-The function workaroundCreepyXServer was adapted from kpowersave-0.7.3 by
-Eivind Magnus Hvidevold <hvidevold@gmail.com>. kpowersave is licensed under
-the GNU GPL, version 2 _only_.
-
 */
 
 #include <X11/Xlib.h>
@@ -36,7 +32,6 @@ the GNU GPL, version 2 _only_.
 #include <stdio.h>
 
 void usage(char *name);
-unsigned long workaroundCreepyXServer(Display *dpy, unsigned long _idleTime );
 
 int main(int argc, char *argv[])
 {
@@ -71,7 +66,7 @@ int main(int argc, char *argv[])
     return 1;
   }
   
-  printf("%lu\n", workaroundCreepyXServer(dpy, ssi->idle));
+  printf("%lu\n", ssi->idle);
 
   XFree(ssi);
   XCloseDisplay(dpy);
@@ -86,58 +81,4 @@ void usage(char *name)
 	  "That is, no command line arguments.  The user's idle time\n"
 	  "in milliseconds is printed on stdout.\n",
 	  name);
-}
-
-/*!
- * This function works around an XServer idleTime bug in the
- * XScreenSaverExtension if dpms is running. In this case the current
- * dpms-state time is always subtracted from the current idletime.
- * This means: XScreenSaverInfo->idle is not the time since the last
- * user activity, as descriped in the header file of the extension.
- * This result in SUSE bug # and sf.net bug #. The bug in the XServer itself
- * is reported at https://bugs.freedesktop.org/buglist.cgi?quicksearch=6439.
- *
- * Workaround: Check if if XServer is in a dpms state, check the 
- *             current timeout for this state and add this value to 
- *             the current idle time and return.
- *
- * \param _idleTime a unsigned long value with the current idletime from
- *                  XScreenSaverInfo->idle
- * \return a unsigned long with the corrected idletime
- */
-unsigned long workaroundCreepyXServer(Display *dpy, unsigned long _idleTime ){
-  int dummy;
-  CARD16 standby, suspend, off;
-  CARD16 state;
-  BOOL onoff;
-
-  if (DPMSQueryExtension(dpy, &dummy, &dummy)) {
-    if (DPMSCapable(dpy)) {
-      DPMSGetTimeouts(dpy, &standby, &suspend, &off);
-      DPMSInfo(dpy, &state, &onoff);
-
-      if (onoff) {
-        switch (state) {
-          case DPMSModeStandby:
-            /* this check is a littlebit paranoid, but be sure */
-            if (_idleTime < (unsigned) (standby * 1000))
-              _idleTime += (standby * 1000);
-            break;
-          case DPMSModeSuspend:
-            if (_idleTime < (unsigned) ((suspend + standby) * 1000))
-              _idleTime += ((suspend + standby) * 1000);
-            break;
-          case DPMSModeOff:
-            if (_idleTime < (unsigned) ((off + suspend + standby) * 1000))
-              _idleTime += ((off + suspend + standby) * 1000);
-            break;
-          case DPMSModeOn:
-          default:
-            break;
-        }
-      }
-    } 
-  }
-
-  return _idleTime;
 }
